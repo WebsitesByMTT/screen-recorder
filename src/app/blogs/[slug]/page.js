f735ebbe292fc2f944169c36844d4ff1f15fad70
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-06-25 05:24:41 -0700

    Blogs Added,KeyWords Linking

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -2,6 +2,7 @@ import { handelArticle } from '@/articledata/Data'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -37,9 +38,8 @@ const page = async ({ params }) => {
          </div>
          <div className='pt-10 md:w-[90%] mx-auto'>
             <div className='flex items-center'>
-               <div className='text-[#B6A4E6]'>Article</div>
+               <Link href={'/blogs'} className='text-[#B6A4E6]'>Blogs</Link>
                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide text-[#B6A4E6] lucide-chevron-right"><path d="m9 18 6-6-6-6" /></svg>
-               <div>Screen Recorder</div>
             </div>
             <div className='pt-3'>
                {data?.title && <h1 className='md:text-[3rem] text-[2rem]'>{data?.title}</h1>}
@@ -50,20 +50,20 @@ const page = async ({ params }) => {
                   <div className='text-black text-[2rem]'>Published on</div>
                   <div className='text-[.9rem] text-black '>{data?.publishDate}</div>
                   {data?.content.length > 0 && (
-                     <div className='py-4 space-y-4'>
+                     <div className='py-3  '>
                         {data?.content?.map((item, index) => (
-                           <div key={index}>
-                              {item.heading && <h1 className='md:text-[3rem] text-[2rem]'>{item.heading}</h1>}
+                           <div key={index} className='space-y-4'>
+                              {item.heading && <h1 className={`${item.size?'text-[1.6rem] md:text-[1.9rem]':'text-[2rem] md:text-[2.5rem] '}`}>{item.heading}</h1>}
                               {item.desc.length > 0 && item?.desc.map((subitem, descIndex) => (
-                                 <p className='py-4' key={descIndex}>{subitem}</p>
+                                 <p className='pb-4' key={descIndex} dangerouslySetInnerHTML={{__html:subitem}}></p>
                               ))}
 
-                              {item?.points.length > 0 && <ul className='space-y-6 pl-6 py-6 list-disc'>
+                              {item?.points.length > 0 && <ul className='space-y-4 pl-6 py-2 list-disc'>
                                  {
                                     item?.points.map((point, ind) => (
                                        <li key={ind}>
-                                          <div className=''>
-                                             <p>  <span className='font-semibold text-[1.1rem] pr-2'>{point.steps}</span>{point.point}</p>
+                                          <div>
+                                             <p>  <span className='font-semibold text-[1.1rem] pr-2' dangerouslySetInnerHTML={{__html:point.steps}}></span><span dangerouslySetInnerHTML={{__html:point.point}}></span></p>
                                           </div>
                                        </li>
                                     ))
